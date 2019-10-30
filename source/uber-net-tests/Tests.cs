5a05d0d565d6beaac743afb9cfd5e17d0e47dfff
lavantgarde <32887314+lavantgarde@users.noreply.github.com>
2019-10-30 12:43:46 -0700

    add tests soln

---
+++ source/uber-net-tests/Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace uber_net_tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
